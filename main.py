92c53549ad27896d57593a36957bf5ee4bf0ffb2
imlonghao <git@imlonghao.com>
2020-03-29 15:12:06 +0800

    Print page length

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 #!/usr/bin/python3
 """ctf web scan
 Author: D4rk
@@ -8,6 +8,7 @@ TODO: 针对某些全站200的情况，分析返回数据包匹配"404 not found
 import sys
 import argparse
 import requests
+
 
 class bcolors:
     """terminal colors"""
@@ -21,6 +22,7 @@ class bcolors:
     WHITE = '\033[37m'
     ENDC = '\033[0m'
 
+
 def dir_scan(url):
     """扫描web敏感目录"""
     with open('dir.txt') as f:
@@ -28,14 +30,16 @@ def dir_scan(url):
     f.close()
     for line in lines:
         try:
-            r = requests.get(url+line, timeout=1)
+            r = requests.get(url + line, timeout=1)
             if r.status_code == 200 or r.status_code == 403:
-                print(bcolors.GREEN + str(r.status_code) + ' ' + bcolors.ENDC + url + line)
+                print(bcolors.GREEN + str(r.status_code) + ' ' +
+                      bcolors.ENDC + url + line + ' ' + str(len(r.text)))
                 # 如果扫描到存在的目录会进行敏感文件扫描
-                file_scan(url+line)
+                file_scan(url + line)
         except requests.exceptions.ConnectionError:
             print("Error connecting to site \"%s\"" % url)
             sys.exit(1)
+
 
 def file_scan(url):
     """扫描web敏感文件"""
@@ -44,14 +48,16 @@ def file_scan(url):
     f.close()
     for line in lines:
         try:
-            r = requests.get(url+line, timeout=1)
+            r = requests.get(url + line, timeout=1)
             if r.status_code == 200:
-                print(bcolors.GREEN + str(r.status_code) + ' ' + bcolors.ENDC + url + line)
+                print(bcolors.GREEN + str(r.status_code) + ' ' +
+                      bcolors.ENDC + url + line + ' ' + str(len(r.text)))
                 # 如果扫描到存在敏感文件会进行编辑器源码泄露扫描
                 source_scan(url, line)
         except requests.exceptions.ConnectionError:
             print("Error connecting to site \"%s\"" % url)
             sys.exit(1)
+
 
 def source_scan(url, filename):
     """扫描编辑器源码泄露"""
@@ -61,12 +67,14 @@ def source_scan(url, filename):
              '%s~' % filename]
     for leak in leaks:
         try:
-            r = requests.get(url+leak, timeout=1)
+            r = requests.get(url + leak, timeout=1)
             if r.status_code == 200:
-                print(bcolors.GREEN + str(r.status_code) + ' ' + bcolors.ENDC + url + leak)
+                print(bcolors.GREEN + str(r.status_code) + ' ' +
+                      bcolors.ENDC + url + leak + ' ' + str(len(r.text)))
         except requests.exceptions.ConnectionError:
             print("Error connecting to site \"%s\"" % url)
             sys.exit(1)
+
 
 def main():
     """主函数"""
@@ -82,7 +90,7 @@ def main():
         url = args.url
         file_scan(url)
         dir_scan(url)
-        print(bcolors.GREEN + 'Scan Complete: %s'%url + bcolors.ENDC)
+        print(bcolors.GREEN + 'Scan Complete: %s' % url + bcolors.ENDC)
     elif args.file:
         with open(args.file) as f1:
             urls = f1.read().splitlines()
@@ -90,11 +98,12 @@ def main():
         for url in urls:
             file_scan(url)
             dir_scan(url)
-            print(bcolors.GREEN + 'Scan Complete: %s'%url + bcolors.ENDC)
+            print(bcolors.GREEN + 'Scan Complete: %s' % url + bcolors.ENDC)
         sys.exit(1)
     else:
         parser.print_usage(sys.stderr)
         sys.exit(1)
+
 
 if __name__ == '__main__':
     main()
